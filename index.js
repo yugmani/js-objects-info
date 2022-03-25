e67c931ecf7b0268f99dab9f3f2f3d536ebd43ed
yugmani <yugmani@yahoo.com>
2022-03-25 13:44:00 -0700

    tasks on objects solved

--- index.js
+++ index.js
@@ -127,13 +127,107 @@ for (let prop in student) {
 //Ordered like an object
 // -------------------------------------
 
-// if we loop over an object, do we get all properties in the same order they were added? 
-// integer properties are sorted, others appear in creation order. 
+// if we loop over an object, do we get all properties in the same order they were added?
+// integer properties are sorted, others appear in creation order.
 //if the keys are non-integer, then they are listed in the creation order,
 //The “integer property” term here means a string that can be converted to-and-from an integer without a change. eg. '49'
 
+// Tasks
+// **************************************
 
+// 1. Hello, object
+// --------------------------------------------
 
+// Write the code, one line for each action:
+// Create an empty object user.
+const myUser = new Object();
+// Add the property name with the value John.
+myUser.name = 'John';
+// Add the property surname with the value Smith.
+myUser.surname = 'Smith';
+console.log(myUser); //{name: "John", surname: "Smith"}
+// Change the value of the name to Pete.
+myUser.name = 'Pete';
+console.log(myUser); //{name: "Pete", surname: "Smith"}
+// Remove the property name from the object.
+delete myUser.name;
+console.log(myUser); //{surname: "Smith"}
+
+// 2. Check for emptiness
+// --------------------------------------------
+
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+const isEmpty = (obj) => {
+  return Object.keys(obj).length === 0 ? 'Yes!' : 'No.';
+};
+
+let schedule = {};
+console.log(`Is it empty? ${isEmpty(schedule)}`); //Yes!
+
+schedule['8:30'] = 'coding time';
+console.log(`Is it empty? ${isEmpty(schedule)}`); //No.
+
+// 3. Sum object properties
+// --------------------------------------------
+
+//We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+let mySalary = {};
+// If salaries is empty, then the result must be 0.
+const sumObjectProperties = (obj) => {
+  if (Object.keys(obj).length !== 0) {
+    let sum = 0;
+    for (let prop in obj) {
+      sum += obj[prop];
+    }
+    return sum;
+  }
+  return 0;
+};
+
+console.log(sumObjectProperties(salaries)); //390
+console.log(sumObjectProperties(mySalary)); //0;
+
+// 4. Multiply numeric property values by 2
+// --------------------------------------------
+
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: 'My menu',
+};
+
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+// P.S. Use typeof to check for a number here.
+const multiplyNumeric = (obj) => {
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      obj[key] = obj[key] * 2;
+    }
+    // obj[key] = obj[key] * 2;  //without checking type string value returns null;
+  }
+};
+
+multiplyNumeric(menu);
+console.log(menu);
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+
+// codewars.com
+// *********************************************
 
 // Instructions
 // Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
